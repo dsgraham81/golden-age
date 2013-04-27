2ff69a8c51d5ce8250ca5f2f6ec67a95f671d05f
bploeckelman <bploeckelman@3ad59573-4299-4ed0-ac8f-1dc5dda5891a>
2013-04-27 18:42:30 +0000

    Initial commit of gdx-setup-ui project

---
+++ goldenage-html/src/com/gamedev/ld26/goldenage/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.gamedev.ld26.goldenage.client;
+
+import com.gamedev.ld26.goldenage.GoldenAgeGame;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new GoldenAgeGame();
+	}
+}
