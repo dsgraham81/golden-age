714cfcfd72301dc7bb8ec19a663ca34a9e5e654e
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2013-04-27 19:30:24 +0000

    Added a new screen, transition between title and play screens

--- goldenage/src/com/gamedev/ld26/goldenage/screens/PlayScreen.java
+++ goldenage/src/com/gamedev/ld26/goldenage/screens/PlayScreen.java
@@ -1,24 +1,22 @@
 package com.gamedev.ld26.goldenage.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.gamedev.ld26.goldenage.GoldenAgeGame;
-import com.gamedev.ld26.goldenage.core.Assets;
 import com.gamedev.ld26.goldenage.utils.Config;
 import com.gamedev.ld26.goldenage.utils.Utils;
 
-public class TitleScreen implements Screen {
+public class PlayScreen implements Screen {
 
 	private final OrthographicCamera camera = new OrthographicCamera();
 	private final GoldenAgeGame game;
 	private float accum = 0.f;
 	
-	public TitleScreen(GoldenAgeGame game) {
+	public PlayScreen(GoldenAgeGame game) {
 		super();
 		this.game = game;
 		camera.setToOrtho(false, Config.window_width, Config.window_height);
@@ -26,9 +24,7 @@ public class TitleScreen implements Screen {
 	
 	public void update() {
 		if (game.input.isKeyDown(Keys.ESCAPE)) {
-			Gdx.app.exit();
-		} else if (Gdx.input.isButtonPressed(Buttons.LEFT)) {
-			game.setScreen(game.play);
+			game.setScreen(game.title);
 		}
 		accum += Gdx.graphics.getDeltaTime();
 	}
@@ -37,25 +33,16 @@ public class TitleScreen implements Screen {
 	public void render(float delta) {
 		update();
 		
-		Gdx.gl20.glClearColor(0.53f, 0.81f, 0.92f, 1);
+		Gdx.gl20.glClearColor(0, 1, 0, 1);
 		Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-		Assets.batch.setProjectionMatrix(camera.combined);
-		Assets.batch.begin();
-		Assets.batch.draw(Assets.background, Config.window_width / 2 - Assets.background.getWidth() / 2, 0);
-		Assets.batch.end();
-		
-		final String testString1 = "Ludem ipsum dare: sit amit, ld48 !26?";
-		final String testString2 = "Testing symbols: ,.?!'\"-+=/\\%()<>:;  -[]- =$= -#- =@=";
 		final int w = 16, h = 16;
 		final float speed = 8.f;
 		final float range = 33.33f;
-		final float minx = Config.window_width / 2 - w * ((testString1.length() + testString2.length()) / 4);
-		float x1 = (float) Math.sin(accum * speed) * range + minx;
-		float x2 = (float) Math.cos(accum * speed / 2) * range + minx;
-
-		Utils.drawText(testString1, x1, Config.window_height / 2 + h / 2, w, h, Color.GREEN);
-		Utils.drawText(testString2, x2, Config.window_height / 2 - h / 2, w, h, Color.ORANGE);
+		final float minx = Config.window_width / 2;
+		float x = (float) Math.sin(accum * speed) * range + minx;
+		float y = (float) Math.cos(accum * speed / 2) * range + minx;
+		Utils.drawText("Play screen", x, y, w, h, Color.BLUE);
 	}
 
 	@Override
@@ -84,5 +71,5 @@ public class TitleScreen implements Screen {
 	public void dispose() {
 		
 	}
-
+	
 }
